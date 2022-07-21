0a370d3a2a4c192f6ca71b7afdfec8ba2161ed24
James Hubert <jwhubert91@gmail.com>
2022-07-21 12:53:28 -0700

    Link add project prompt to create project page

--- src/components/AddProjectPrompt.tsx
+++ src/components/AddProjectPrompt.tsx
@@ -1,15 +1,22 @@
 import Button from "./Button"
 import { MdAddCircle } from "react-icons/md"
+import { useNavigate } from "react-router-dom"
+import { routes } from "../utilities/routes"
 
 interface AddProjectPromptProps {
   className?: string
 }
 
 function AddProjectPrompt({ className = "" }: AddProjectPromptProps) {
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate(routes.createProject)
+  }
   return (
     <Button
       buttonStyle="PILL"
       className={`w-full sm:py-2 bg-white text-black hover:text-white border border-2 border-xiketicBlack hover:border-snowWhite ${className}`}
+      onClick={handleClick}
     >
       <div className="flex items-center">
         <MdAddCircle className="mr-2 text-3xl sm:text-5xl" />
