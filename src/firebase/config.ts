35ea7f31d9275d84c834acbe8df26f90e3821690
James Hubert <jwhubert91@gmail.com>
2022-07-26 15:02:05 -0700

    Add firebase config file and export db

---
+++ src/firebase/config.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA91cgAZ4jcRDbty1JQ2_opOSsstSRndak",
+  authDomain: "portful-v1.firebaseapp.com",
+  projectId: "portful-v1",
+  storageBucket: "portful-v1.appspot.com",
+  messagingSenderId: "913556072636",
+  appId: "1:913556072636:web:207eda56f40026528608b9",
+  measurementId: "G-M06PVMM7BE"
+};
+
+// init firebase
+initializeApp(firebaseConfig)
+
+// init firestore
+const db = getFirestore()
+
+export { db };
