2449daffbdf0db33baed4ffe7a590011a7680631
James Hubert <jwhubert91@gmail.com>
2022-07-27 15:23:25 -0700

    Start on context

---
+++ src/context/AuthContext.ts
@@ -0,0 +1,9 @@
+// Continue Firebase work at lesson 203, original set up at lesson 121
+import { createContext, useReducer } from "react";
+
+// @ts-ignore
+export const AuthContext = createContext()
+
+export const AuthContextProvider = ()=> {
+  return null
+}
---
+++ src/hooks/useAuthContext.ts
@@ -0,0 +1,3 @@
+export const useAuthContext = ()=> {
+  return null
+}
