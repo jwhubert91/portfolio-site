2e05f397c8479210fdc229f591b1ea532d109861
James Hubert <jwhubert91@gmail.com>
2022-08-08 23:51:25 -0700

    add ProjectCard back to ProjectList component

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom"
 import { makePath } from "../utilities/helpers"
 import { routes } from "../utilities/routes"
 import Card from "./Card"
+import ProjectCard from "./ProjectCard"
 
 interface SimpleProjectProps {
   id: string
@@ -18,13 +19,15 @@ function ProjectList({ projects }: ProjectListProps) {
       {projects.map((project) => {
         const path = makePath(routes.projects, routes.edit, project.id)
         return (
-          <Card key={project.id} className="mb-4">
-            <Link to={path}>
-              <div className="w-full p-2">
-                <p>{project.title}</p>
-              </div>
-            </Link>
-          </Card>
+          // <Card key={project.id} className="mb-4">
+          //   <Link to={path}>
+          //     <div className="w-full p-2">
+          //       <p>{project.title}</p>
+          //     </div>
+          //   </Link>
+          // </Card>
+          // TODO: Pass actual props to ProjectCard component :)
+          <ProjectCard key={project.id} />
         )
       })}
     </div>
