07f58485cf92e6a7cb8b4db64f2e7bdbc98187fe
James Hubert <jwhubert91@gmail.com>
2022-09-09 22:23:04 -0700

    Change out firebase config variables and add env file to gitignore

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,14 +3,15 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { getStorage } from 'firebase/storage'
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyA91cgAZ4jcRDbty1JQ2_opOSsstSRndak",
-  authDomain: "portful-v1.firebaseapp.com",
-  projectId: "portful-v1",
-  storageBucket: "portful-v1.appspot.com",
-  messagingSenderId: "913556072636",
-  appId: "1:913556072636:web:207eda56f40026528608b9",
-  measurementId: "G-M06PVMM7BE"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // init firebase
