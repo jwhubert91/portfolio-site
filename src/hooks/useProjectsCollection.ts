2d41e2e7da241533b2ed8316fb2f66575e428670
James Hubert <jwhubert91@gmail.com>
2022-09-16 18:44:08 -0400

    Add filter to show only that users project on portfolio page

--- src/hooks/useProjectsCollection.ts
+++ src/hooks/useProjectsCollection.ts
@@ -2,15 +2,16 @@ import { useEffect, useState } from "react"
 
 // firebase
 import { db } from "../firebase/config"
-import { collection, onSnapshot, query, orderBy } from "firebase/firestore"
+import { collection, onSnapshot, query, orderBy, where } from "firebase/firestore"
 import { ProjectType } from "../utilities/types"
 
-export const useProjectsCollection = (collectionName: string)=> {
+export const useProjectsCollection = (collectionName: string, displayName?: string)=> {
   const [documents, setDocuments] = useState<ProjectType[]>([])
+  console.log(`Entered useProjectsCollection hook with collectionName ${collectionName} and displayName ${displayName}`)
 
   useEffect(()=> {
     let ref = collection(db, collectionName)
-    const q = query(ref, orderBy("timestamp", "desc"))
+    const q = displayName ? query(ref, where("creatorDisplayname", "==", displayName), orderBy("timestamp", "desc")) : query(ref, orderBy("timestamp", "desc"))
 
     const unsub = onSnapshot(q, (snapshot)=> {
       // TODO: When the collection objects are finalized, type these two ignored values
@@ -20,11 +21,13 @@ export const useProjectsCollection = (collectionName: string)=> {
         results.push({...doc.data(), id: doc.id})
       })
       // @ts-ignore
+      console.log(results)
+      // @ts-ignore
       setDocuments(results)
     })
 
     return ()=> unsub()
-  },[collectionName])
+  },[collectionName, displayName])
 
   return { documents }
 }
